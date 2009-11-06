6ea9dfbcac693b3a6ddf07769334da70f7d1f28f
Evan Carroll <me@evancarroll.com>
2009-11-06 12:24:56 -0600

    now our UA defaults to json

--- lib/GoogleAPI/UserAgent.pm
+++ lib/GoogleAPI/UserAgent.pm
@@ -1,5 +1,25 @@
 package GoogleAPI::UserAgent;
+use strict;
+use warnings;
 
-use parent 'LWP::UserAgent';
+use Moose;
+use LWP::UserAgent;
+
+has 'ua' => (
+	isa => 'LWP::UserAgent'
+	, is => 'ro'
+	, default => sub { LWP::UserAgent->new }
+	, handles => qr/.*/
+	, initializer => sub {
+		my ( $self, $value, $writerSub, $attributeMeta ) = @_;
+		$value->add_handler( 'request_prepare' => sub {
+			my ( $request, $ua, $h ) = @_;
+			my %query_form = $request->uri->query_form;
+			$query_form{alt} //= 'json';
+			$request->uri->query_form( \%query_form );
+		} );
+		$writerSub->( $value );
+	}
+);
 
 1;
