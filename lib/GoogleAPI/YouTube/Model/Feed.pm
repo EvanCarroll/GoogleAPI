d4dd4334f7408476b8191d2a0defaf48a5962916
Evan Carroll <me@evancarroll.com>
2009-11-12 10:14:25 -0600

    added link_next, and link_previous for pagination, moved stuff from feed default to entry default

--- lib/GoogleAPI/YouTube/Model/Feed.pm
+++ lib/GoogleAPI/YouTube/Model/Feed.pm
@@ -12,9 +12,45 @@ has 'entries' => (
 	isa  => 'ArrayRef[GoogleAPI::YouTube::Model::Entry]'
 	, is => 'ro'
 	, default => sub { +[] }
-	, traits  => ['Array']
-	, handles => {
-		add_entry => 'push'
+	#	, traits  => ['Array']
+	#	, handles => { add_entry => 'push' }
+	, lazy => 1
+	, default => sub {
+		my $self = shift;
+	
+		my @entries;
+		foreach my $entry ( @{$self->_feed->{entry}} ) {
+			my $e = GoogleAPI::YouTube::Model::Entry->new( @_ );
+			$self->new_entry({
+				## Base from YouTube::Model
+				name          => $entry->{author}[0]{name}{'$t'}
+				, uri         => $entry->{author}[0]{uri}{'$t'}
+				, id          => $entry->{id}{'$t'}
+				, updated     => $entry->{updated}{'$t'}
+				, title       => $entry->{title}{'$t'}
+
+				## Rest of the stuff
+				, published   => $entry->{published}{'$t'}
+				, content     => $entry->{content}{'$t'}
+				
+				, yt_videoid  => $entry->{'media$group'}{'yt$videoid'}
+				, yt_duration => $entry->{'media$group'}{'yt$duration'}{'seconds'}
+				, yt_uploaded => $entry->{'media$group'}{'yt$uploaded'}{'$t'}
+
+				, media_title       => $entry->{'media$group'}{'media$title'}{'$t'}
+				, media_credit      => $entry->{'media$group'}{'media$credit'}[0]{'$t'}
+				, media_description => $entry->{'media$group'}{'media$description'}{'$t'}
+				, media_keywords    => $entry->{'media$group'}{'media$keywords'}{'$t'}
+				
+				## XXX in author feed, this is an array, in query feed it is a single element
+				## http://code.google.com/p/gdata-issues/issues/detail?id=1597
+				## , media_player      => $entry->{'media$group'}{'media$player'}{'url'}
+			});
+			push @entries, $e;
+		}
+
+		\@entries;
+
 	}
 );
 
@@ -31,42 +67,10 @@ has [qw/
 has '_feed' => (
 	isa  => 'HashRef'
 	, is => 'ro'
-	, trigger => sub {
-		my ( $self, $feed ) = @_;
-		
-		foreach my $entry ( @{$feed->{entry}} ) {
-			$self->new_entry({
-				name          => $entry->{author}[0]{name}{'$t'}
-				, uri         => $entry->{author}[0]{uri}{'$t'}
-				, title       => $entry->{title}{'$t'}
-				, published   => $entry->{published}{'$t'}
-				, updated     => $entry->{updated}{'$t'}
-				, id          => $entry->{id}{'$t'}
-				, content     => $entry->{content}{'$t'}
-				
-				, yt_videoid  => $entry->{'media$group'}{'yt$videoid'}
-				, yt_duration => $entry->{'media$group'}{'yt$duration'}{'seconds'}
-				, yt_uploaded => $entry->{'media$group'}{'yt$uploaded'}{'$t'}
-
-				, media_title       => $entry->{'media$group'}{'media$title'}{'$t'}
-				, media_credit      => $entry->{'media$group'}{'media$credit'}[0]{'$t'}
-				, media_description => $entry->{'media$group'}{'media$description'}{'$t'}
-				, media_keywords    => $entry->{'media$group'}{'media$keywords'}{'$t'}
-				
-				## XXX in author feed, this is an array, in query feed it is a single element
-				## http://code.google.com/p/gdata-issues/issues/detail?id=1597
-				## , media_player      => $entry->{'media$group'}{'media$player'}{'url'}
-			});
-		}
-
-	}
 );
 
-sub new_entry {
-	my $self = shift;
-	my $e = GoogleAPI::YouTube::Model::Entry->new( @_ );
-	$self->add_entry( $e );
-}
+has 'link_next' => ( isa => 'Maybe[URI]', is => 'ro' );
+has 'link_previous' => ( isa => 'Maybe[URI]', is => 'ro' );
 
 sub BUILDARGS {
 	my $class = shift;
@@ -101,10 +105,13 @@ sub BUILDARGS {
 			, generator_version => $feed->{generator}{version}
 	
 			, opensearch_total_results  => $feed->{'openSearch$totalResults'}{'$t'}
-				,	opensearch_start_index    => $feed->{'openSearch$startIndex'}{'$t'}
-				,	opensearch_items_per_page => $feed->{'openSearch$itemsPerPage'}{'$t'}
+			, opensearch_start_index    => $feed->{'openSearch$startIndex'}{'$t'}
+			, opensearch_items_per_page => $feed->{'openSearch$itemsPerPage'}{'$t'}
+			
+			, link_next     => ( map URI->new($_->{href}), grep $_->{rel} eq 'next', @{$feed->{'link'}} )
+			, link_previous => ( map URI->new($_->{href}), grep $_->{rel} eq 'previous', @{$feed->{'link'}} )
 
-		}
+		};
 	
 	}
 	else {
